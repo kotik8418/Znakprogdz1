15907522329ebb2d3abb605fa475d8c6810ee059
Kotik <Kotik8418@gmail.com>
2022-08-21 19:52:57 +0300

    Задача 6

---
+++ Task006/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("Введите число");
+
+double A = Convert.ToDouble(Console.ReadLine());
+
+if ((A % 2)==0)
+{
+  Console.WriteLine("Четное число");  
+}
+else
+
+{
+    Console.WriteLine("Нечётное число");
+}

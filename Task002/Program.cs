0704e4fbf161677fc2ecb486888b5d8ae8acf445
Kotik <Kotik8418@gmail.com>
2022-08-21 18:53:25 +0300

    Задача 2

---
+++ Task002/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число 1");
+int a1 =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+int a2 =Convert.ToInt32(Console.ReadLine());
+
+if (a1 > a2 )
+{
+    Console.WriteLine("a1 " + (a1));
+}
+
+else
+{
+  Console.WriteLine("a2 " + (a2)); 
+}

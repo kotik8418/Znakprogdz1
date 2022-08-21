daa227c22e87842d4c38bf5b02ae1aafd55407dc
Kotik <Kotik8418@gmail.com>
2022-08-21 20:14:50 +0300

    Задача 5

---
+++ Task005/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("введите число A = ");
+int A = Convert.ToInt32(Console.ReadLine());
+            for (int i = -A; i <= A-1; i++)
+           {
+                Console.Write(i + ",");
+            }
+            Console.Write(A);
